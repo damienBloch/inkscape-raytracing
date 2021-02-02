207530daa0f6d558efe082bd69fb93fc2d619d7b
BLOCH Damien <bloch.dam@gmail.com>
2021-02-02 23:44:03 +0100

    Normal on same side as incoming beam

--- raytracing/shade.py
+++ raytracing/shade.py
@@ -17,3 +17,13 @@ class ShadeRec(object):
     def __repr__(self):
         return f"ShadeRec({self.hit_an_object}, {self.local_hit_point}, " \
                f"{self.normal}, {self.travel_dist})"
+
+    def set_normal_same_side(self, point: np.ndarray):
+        if self.normal is None:
+            raise RuntimeError("Can't find normal orientation if not "
+                               "already defined.")
+        elif self.local_hit_point is None:
+            raise RuntimeError("Can't find normal orientation if hit point "
+                               "not defined.")
+        elif np.dot(self.normal, self.local_hit_point - point) > 0:
+            self.normal = -self.normal
