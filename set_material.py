3bde12684d3554a09386c1d6675ee193e7aee476
BLOCH Damien <bloch.dam@gmail.com>
2021-02-08 20:58:53 +0100

    Name alias

--- set_material.py
+++ set_material.py
@@ -39,12 +39,22 @@ class SetMaterial(inkex.Effect):
             self.process_object(obj)
 
     def process_shape(self, obj: inkex.ShapeElement):
+        """
+        In the description of the element, replaces the optical properties
+        with the new one.
+        """
+        name_alias = {
+                "None": None, "Beam": 'beam', "Mirror": 'mirror',
+                "Beam dump": 'beam_dump', "Beam splitter": 'beam_splitter',
+                "Glass": 'glass'
+        }
         desc = get_description(obj)
         new_desc = clear_description(desc)
-        material_name = self.options.optical_material
-        if material_name is not 'none':
+        material_name = name_alias[self.options.optical_material]
+        if material_name is not None:
             new_desc += f'optics:{material_name}'
-        inkex.utils.debug(new_desc)
+            if material_name == 'glass':
+                new_desc += f':{self.options.optical_index}'
         set_description(obj, new_desc)
 
 
