2dc22adb0286bc3eaeb2ebb5e9cfaeb4ad6125e0
BLOCH Damien <bloch.dam@gmail.com>
2021-02-26 17:11:11 +0100

    beam test

---
+++ tests/integration/test_beam.py
@@ -0,0 +1,11 @@
+from inkex.tester import ComparisonMixin, TestCase
+
+from render_raytracing import Tracer
+
+
+class BeamTest(ComparisonMixin, TestCase):
+    effect_class = type("test_beam", (Tracer,), {})
+    compare_file = 'svg/beams.svg'
+    comparisons = [
+            ('--id=g2013',),
+    ]
