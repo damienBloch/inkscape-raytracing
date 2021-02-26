99b1b27a727b4bb35796b24428a8fe2d1a3700f2
BLOCH Damien <bloch.dam@gmail.com>
2021-02-26 17:15:28 +0100

    glass test

---
+++ tests/integration/test_material_glass.py
@@ -0,0 +1,11 @@
+from inkex.tester import ComparisonMixin, TestCase
+
+from render_raytracing import Tracer
+
+
+class GlassTest(ComparisonMixin, TestCase):
+    effect_class = type("glass_test", (Tracer,), {})
+    compare_file = 'svg/glass.svg'
+    comparisons = [
+            ('--id=g1271',),
+    ]
