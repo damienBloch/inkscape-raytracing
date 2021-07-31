6eff52c660399ca1b25dff533e292360b2d500ba
Damien Bloch <bloc.dam@gmail.com>
2021-07-31 19:32:28 +0200

    Change OpticMaterial to Protocol

--- raytracing/material/optic_material.py
+++ raytracing/material/optic_material.py
@@ -1,20 +1,19 @@
-import abc
-from typing import List
+from abc import abstractmethod
+from typing import Protocol, List
 
 from raytracing.ray import Ray
 from raytracing.shade import ShadeRec
 
 
-class OpticMaterial(abc.ABC):
-    """Abstract class for an optical material"""
+class OpticMaterial(Protocol):
+    """Protocol for an optical material"""
 
-    def __init__(self):
-        pass
-
-    @abc.abstractmethod
+    @abstractmethod
     def generated_beams(self, ray: Ray, shade: ShadeRec) -> List[Ray]:
+        """Compute the beams generated after intersection of a beam with this
+        material
+
+        Returns list of new beam seeds to start from after the intersection
+        of a beam and an object.
         """
-        :return: list of new beam seeds to start from after the intersection
-        of a beam and an object
-        """
-        pass
+        raise NotImplementedError
