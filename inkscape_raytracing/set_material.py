b014bacb542a7519de56e1f427af0570530e6a8f
Damien Bloch <bloc.dam@gmail.com>
2021-08-21 20:39:11 +0200

    Change type dispatch to set description to singledispatchmethod

--- inkscape_raytracing/set_material.py
+++ inkscape_raytracing/set_material.py
@@ -1,3 +1,8 @@
+from __future__ import annotations
+
+from functools import singledispatchmethod
+from typing import Final
+
 import inkex
 
 from utils import clear_description
@@ -6,19 +11,19 @@ from utils import clear_description
 class SetMaterial(inkex.Effect):
     """Writes the chosen optical property in the element description"""
 
+    # only change the description for these objects
+    filter_primitives: Final = (
+        inkex.PathElement,
+        inkex.Line,
+        inkex.Polyline,
+        inkex.Polygon,
+        inkex.Rectangle,
+        inkex.Ellipse,
+        inkex.Circle,
+    )
+
     def __init__(self):
         super().__init__()
-
-        # only change the description for these objects
-        self._filter_primitives = (
-            inkex.PathElement,
-            inkex.Line,
-            inkex.Polyline,
-            inkex.Polygon,
-            inkex.Rectangle,
-            inkex.Ellipse,
-            inkex.Circle,
-        )
 
     def add_arguments(self, pars):
         pars.add_argument(
@@ -30,47 +35,49 @@ class SetMaterial(inkex.Effect):
         pars.add_argument("--optical_index", type=float, default=1.5168)
 
     def effect(self) -> None:
-        filter_ = self._filter_primitives + (inkex.Group,)
+        filter_ = self.filter_primitives + (inkex.Group,)
         for obj in self.svg.selection.filter(filter_).values():
-            self.process_object(obj)
+            self.update_description(obj)
 
-    def process_object(self, obj: inkex.BaseElement) -> None:
-        if isinstance(obj, inkex.Group):
-            self.process_group(obj)
-        elif isinstance(obj, self._filter_primitives):
-            self.process_shape(obj)
+    @singledispatchmethod
+    def update_description(self, arg):
+        pass
 
-    def process_group(self, group: inkex.Group) -> None:
-        """Splits the objects inside a group and treats them individually"""
-
+    @update_description.register
+    def _(self, group: inkex.Group):
         for obj in group:
-            self.process_object(obj)
+            self.update_description(obj)
 
-    def process_shape(self, obj: inkex.ShapeElement):
-        """
-        In the description of the element, replaces the optical properties
-        with the new one.
-        """
+    for type in filter_primitives:
 
-        desc = obj.desc
-        if desc is None:
-            desc = ""
-        new_desc = clear_description(desc)
+        @update_description.register(type)
+        def _(self, obj):
+            """
+            In the description of the element, replaces the optical properties
+            with the new one.
+            """
 
-        name_alias = {
-            "None": None,
-            "Beam": "beam",
-            "Mirror": "mirror",
-            "Beam dump": "beam_dump",
-            "Beam splitter": "beam_splitter",
-            "Glass": "glass",
-        }
-        material_name = name_alias[self.options.optical_material]
-        if material_name is not None:
-            new_desc += f"optics:{material_name}"
-            if material_name == "glass":
-                new_desc += f":{self.options.optical_index:.4f}"
-        obj.desc = new_desc
+            desc = obj.desc
+            if desc is None:
+                desc = ""
+            elif desc != "":
+                desc += "\n"
+            new_desc = clear_description(desc)
+
+            name_alias = {
+                "None": None,
+                "Beam": "beam",
+                "Mirror": "mirror",
+                "Beam dump": "beam_dump",
+                "Beam splitter": "beam_splitter",
+                "Glass": "glass",
+            }
+            material_name = name_alias[self.options.optical_material]
+            if material_name is not None:
+                new_desc += f"optics:{material_name}"
+                if material_name == "glass":
+                    new_desc += f":{self.options.optical_index:.4f}"
+            obj.desc = new_desc
 
 
 if __name__ == "__main__":
